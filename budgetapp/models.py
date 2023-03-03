c4c13d5dbc2db739edda4b673ff0220beb849254
JBarrett9 <107080836+JBarrett9@users.noreply.github.com>
2023-03-03 16:44:56 -0500

    Update models.py

--- budgetapp/models.py
+++ budgetapp/models.py
@@ -11,7 +11,7 @@ def load_user(user_id):
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, unique=True)
     name = db.Column(db.String(20), nullable=False)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
